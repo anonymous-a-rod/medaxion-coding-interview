d1bf49b20d746c6dc2c44cc605e2e9df285d451f
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2024-11-05 07:10:18 -0500

    change reference from instance variable to getter methods on candidate

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -11,42 +11,42 @@ class Candidate
   end
 
   def to_s
-    info = "Name: #{@name}\nEmail: #{@email}\nSkills: #{@skills&.join(', ')}"
-    info += "\nSkill Levels: #{format_skill_levels}" unless @skill_levels.empty?
-    info += "\nInterview Dates: #{format_interview_dates}" unless @interview_dates.empty?
+    info = "Name: #{name}\nEmail: #{email}\nSkills: #{skills&.join(', ')}"
+    info += "\nSkill Levels: #{format_skill_levels}" unless skill_levels.empty?
+    info += "\nInterview Dates: #{format_interview_dates}" unless interview_dates.empty?
     info
   end
 
   def add_skills(*args)
-    args.each { |skill| @skills << skill }
+    args.each { |skill| skills << skill }
   end
 
   def rate_skill(skill, level)
-    unless @skills.include? skill
+    unless skills.include? skill
       raise ArgumentError, "#{skill} is not part of the candidate's skills."
     end
     unless (1..5).include? level
       raise ArgumentError, 'Skill level must be between 1 and 5.'
     end
 
-    @skill_levels[skill] = level
+    skill_levels[skill] = level
   end
 
   def clear_skill_rating(skill)
-    @skill_levels.delete skill
+    skill_levels.delete skill
   end
 
   def has_interviews_scheduled?
-    !@interview_dates.empty?
+    !interview_dates.empty?
   end
 
   private
 
   def format_skill_levels
-    @skill_levels.map { |skill, level| "#{skill}: #{level}" }.join(', ')
+    skill_levels.map { |skill, level| "#{skill}: #{level}" }.join(', ')
   end
 
   def format_interview_dates
-    @interview_dates.join(', ')
+    interview_dates.join(', ')
   end
 end
